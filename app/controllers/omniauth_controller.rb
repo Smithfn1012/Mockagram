5e8b032b8b1dfd83d8279196ee95a53740c179a3
Smithfn1012 <58006468+Smithfn1012@users.noreply.github.com>
2020-05-19 13:31:39 -0500

    finished adding github to signin

--- app/controllers/omniauth_controller.rb
+++ app/controllers/omniauth_controller.rb
@@ -12,8 +12,9 @@ class OmniauthController < ApplicationController
 
     def github
         @user = User.create_from_provider_data(request.env['omniauth.auth'])
+        
         if @user.persisted?
-            sign_in_and_redirect_user @user
+            sign_in_and_redirect @user
         else
             flash[:error] = 'There was a problem signing you in through Github. Please register or try signing in later.'
             redirect_to new_user_registration_url
