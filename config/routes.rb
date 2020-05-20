d0d041ab82a5546e5bdf60bae1e2ef8c73de9ef6
Smithfn1012 <58006468+Smithfn1012@users.noreply.github.com>
2020-05-19 19:32:03 -0500

    adding comments controller and comments to routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   root to: 'home#index'
 
   devise_for :users, controllers: {omniauth_callbacks: 'omniauth'}
-   resources :users, only: [:show, :edit, :update]
-   resources :posts, only: [:new, :create, :show, :destroy]
+  resources :users, only: [:show, :edit, :update]
+  resources :posts, only: [:new, :create, :show, :destroy] do
+    resources :comments
+  end
   get 'search' => 'search#index'
 end
